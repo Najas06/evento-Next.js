7e4acc50d0ae9d5582b4576d7c54c4e32dae84c5
Najas06 <najasneju@gmail.com>
2024-11-18 22:23:30 +0530

    event page completed

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import H1 from "@/components/h1";
+import Image from "next/image";
 
-const page = () => {
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+const page = async ({ params }: EventPageProps) => {
+  const slug = params.slug;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+  // console.log(data);
   return (
-    <div>Event Page</div>
-  )
+    <main>
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
+        <Image
+          src={event.imageUrl}
+          alt="event background"
+          className="object-cover z-0 blur-3xl"
+          fill
+          quality={50}
+          sizes="(max-width: 1280px) 100vw, 1280px"
+          priority
+        />
+        <div className="z-1 relative gap-6 lg:gap-x-16 flex flex-col lg:flex-row">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+            className="rounded-xl border-2 border-white/50 object-cover"
+          />
+          <div className="flex flex-col">
+            <p className="text-white/75">
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+            <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto w-[95vw] rounded-md border-white/10 border-2 sm:w-full bg-blur py-2 state-effects">
+              Get Tickets
+            </button>
+          </div>
+        </div>
+      </section>
+      <div className="text-center px-5 py-16">
+        <Section>
+          <SectionHeading>About this event</SectionHeading>
+          <SectionContent>{event.description}</SectionContent>
+        </Section>
+        <Section>
+          <SectionHeading>Location</SectionHeading>
+          <SectionContent>{event.location}</SectionContent>
+        </Section>
+      </div>
+    </main>
+  );
+};
+
+export default page;
+
+function Section({ children }: { children: React.ReactNode }) {
+  return <section className="mb-12">{children}</section>;
 }
 
-export default page
+function SectionHeading({ children }: { children: React.ReactNode }) {
+  return <h2 className="text-2xl mb-8">{children}</h2>;
+}
+
+function SectionContent({ children }: { children: React.ReactNode }) {
+  return (
+    <p className="text-lg leading-8 text-white/75 max-w-4xl mx-auto">
+      {children}
+    </p>
+  );
+}
--- src/components/h1.tsx
+++ src/components/h1.tsx
@@ -1,12 +1,20 @@
-type H1Props = {
-    children: React.ReactNode
-  }
-const H1 = ({children}:H1Props) => {
-  return (
-    <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
-        {children}
-      </h1>
-  )
-}
+import { cn } from "@/lib/utils";
 
-export default H1
+type H1Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+const H1 = ({ children, className }: H1Props) => {
+  return (
+    <h1
+      className={cn(
+        "text-3xl lg:text-6xl font-bold tracking-tight",
+        className
+      )}
+    >
+      {children}
+    </h1>
+  );
+};
+
+export default H1;
